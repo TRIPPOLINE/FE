1271537cef60b898e8507a6ed14371edb5684725
0taetae <teayoung0812@naver.com>
2024-11-22 09:00:35 +0900

    포트번호변경

--- src/stores/noticeStore.js
+++ src/stores/noticeStore.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: 'http://localhost:8080/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
